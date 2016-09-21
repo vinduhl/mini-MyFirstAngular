2a17715431ea7bc64432b27a7f1f10feaa4ffaa4
Wendell Valdez <vinduhl@gmail.com>
2016-09-20 19:26:17 -0500

    New files

---
+++ js/mainCtrl.js
@@ -0,0 +1,12 @@
+angular.module("friendsList")
+  .controller("mainCtrl", function($scope) {
+
+  //$scope.friends = friends;
+  $scope.friends = ["Gary", "Chris", "Valerie", "Lee", "Diane", "Daniel", "Laura", "Chad", "Regan", "Michael"];
+
+  });
+
+
+var friends = [
+  "Gary", "Chris", "Valerie", "Lee", "Diane", "Daniel", "Laura", "Chad", "Regan", "Michael"
+];
